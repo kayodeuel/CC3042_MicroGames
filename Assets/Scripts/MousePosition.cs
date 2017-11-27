7d94a2dfa90bd6d81c50cf9202aecfbf8dc29793
Unknown <kayode@uel.ac.uk>
2017-11-27 08:24:58 +0000

    Week 9 collect game

---
+++ Assets/Scripts/MousePosition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour {
+
+    Vector3 mousePosition; 
+        
+    Vector3 targetPosition;
+
+    AudioSource sound;
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+        //Gets the AudioSource component at the start of the scene
+        sound = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Stores the current mouse position
+        mousePosition = Input.mousePosition;
+        //Converts the current mouse position to world space and stores it in targetPosition
+        targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10));
+        //Sets the position of the object with this script attached to the targetPosition
+        transform.position = targetPosition;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        sound.Play();
+        Destroy(other.gameObject);
+    }
+}
